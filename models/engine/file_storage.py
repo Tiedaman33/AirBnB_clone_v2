3456926c902131082bf93d31d59b417ce152cb34
Tiedaman33 <orinabravin6@gmail.com>
2024-04-20 15:01:30 -0700

    sql

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,8 +9,9 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+
 class FileStorage:
-     """Represent an abstracted storage engine.
+    """Represent an abstracted storage engine.
 
     Attributes:
         __file_path (str): The name of the file to save objects to.
@@ -23,8 +24,7 @@ class FileStorage:
     def all(self, cls=None):
         """Return a dictionary of instantiated objects in __objects.
 
-        If a cls is specified, returns
-        a dictionary of objects of that type.
+        If a cls is specified, returns a dictionary of objects of that type.
         Otherwise, returns the __objects dictionary.
         """
         if cls is not None:
