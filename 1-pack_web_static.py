532fd4080ec77ee128138353764ca34a3151589b
Tiedaman33 <orinabravin6@gmail.com>
2024-05-03 14:19:15 -0700

    clone

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-"""
-Fabric script that generates a .tgz archive from the contents of the web_static folder
-"""
-
-from fabric.api import local
+# Fabfile to generates a .tgz archive from the contents of web_static.
+import os.path
 from datetime import datetime
+from fabric.api import local
+
 
 def do_pack():
-    """
-    Generates a .tgz archive from the contents of the web_static folder
-
-    Returns:
-        str: Path to the archive if generated successfully, None otherwise
-    """
-    now = datetime.now()
-    timestamp = now.strftime("%Y%m%d%H%M%S")
-    archive_name = "web_static_" + timestamp + ".tgz"
-    local("mkdir -p versions")
-    result = local("tar -cvzf versions/{} web_static".format(archive))
-    if result is not None:
-        return archive
-    else:
+    """Create a tar gzipped archive of the directory web_static."""
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
-
-if __name__ == "__main__":
-    do_pack()
-
+    return file
